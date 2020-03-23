0b55c08e77ea3667a0c2369f5b46383a16e1eac3
machadokd <machadokd@users.noreply.github.com>
2020-03-23 16:32:06 +0000

    JoãoMachado-18578

---
+++ Aula 5/PongGame-JoãoMachado-18578/Assets/Scripts/Loss.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loss : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ballTag"))
+        {
+            GameObject.Find("ball").GetComponent<Ball>().Reset();
+            GameObject.Find("gameManagerObj").GetComponent<Manager>().Reset();
+            GameObject.Find("player1").GetComponent<Paddle>().Reset();
+
+        }
+    }
+}
---
+++ Aula 5/PongGame-JoãoMachado-18578/Assets/Scripts/Paddle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+
+    private Vector3 startPosition;
+
+    public float movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position; //guarda primeira posição
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed);
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = startPosition;
+    }
+}
+ 
